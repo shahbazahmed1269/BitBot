d7e1a23f5a60e05c53fbbcd869d0954b642438b5
shahbaz ahmed <shahbazahmed1269@gmail.com>
2018-09-10 23:26:07 +0530

    Updated unocoin endpoint

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const unirest = require('unirest');
 const express = require("express");
 const Botly = require("botly");
-const buyEndPoint = "https://www.unocoin.com/trade?buy"
-const sellEndPoint = "https://www.unocoin.com/trade?sell"
+
+const buyEndPoint = "https://api.unocoin.com/api/trades/btc/buy"
+const sellEndPoint = "https://api.unocoin.com/api/trades/btc/sell"
+const port = 8080
 
 var getRate = function (endPoint, callback) {
   unirest.get(endPoint).end(function (response) {
-  callback(null, parseInt(response.body));
+    if (endPoint == buyEndPoint) {
+      callback(null, parseInt(response.body["buying_price"]));
+    }
+    else {
+      callback(null, parseInt(response.body["selling_price"]));
+    }
   });
 }
-// Route handling using express
+
 var app = express();
-// buy endpoint
+
 app.get("/buy", function (req, res) {
   getRate(buyEndPoint, function (err, rate) {
     if (err == null) {
-    res.json({"buy": rate});
-    } else {
-      res.status(500).send('Something went wrong!');
-    }
-  })
-});
-// sell endpoint
-app.get("/sell", function (req, res) {
-  getRate(sellEndPoint, function (err, rate) {
-    if (err == null) {
-    res.json({"sell": rate});
+      res.json({ "buy": rate });
     } else {
       res.status(500).send('Something went wrong!');
     }
   })
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000 ...');
+app.get("/sell", function (req, res) {
+  getRate(sellEndPoint, function (err, rate) {
+    if (err == null) {
+      res.json({ "sell": rate });
+    } else {
+      res.status(500).send('Something went wrong!');
+    }
+  })
+});
+
+app.listen(port, function () {
+  console.log(`listening on port: ${port} ...`);
 })

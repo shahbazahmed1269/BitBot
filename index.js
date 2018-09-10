4a6a29b5c7b0fcb56351a8565506d62adc87e73d
shahbaz ahmed <shahbazahmed1269@gmail.com>
2018-09-11 00:23:49 +0530

    Replaced package unirest with request due to security vulnurability report

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-const unirest = require('unirest');
-const express = require("express");
-const Botly = require("botly");
+var request = require('request')
+const express = require("express")
 
 const buyEndPoint = "https://api.unocoin.com/api/trades/btc/buy"
 const sellEndPoint = "https://api.unocoin.com/api/trades/btc/sell"
-const port = 8080
+const port = 80
 
 var getRate = function (endPoint, callback) {
-  unirest.get(endPoint).end(function (response) {
-    if (endPoint == buyEndPoint) {
-      callback(null, parseInt(response.body["buying_price"]));
+  request(endPoint, {json: true}, (error, res, body) => {
+    if(error) { 
+      callback(error, 0)
+      return
+    } 
+    
+    if (res.statusCode == 200) {
+      if (endPoint == buyEndPoint) {
+        callback(null, parseInt(body["buying_price"]))
+      }
+      else {
+        callback(null, parseInt(body["selling_price"]))
+      }
+    } else {
+      console.log(`error occured: ${body}`)
+      callback(body['error']['message'])
     }
-    else {
-      callback(null, parseInt(response.body["selling_price"]));
-    }
-  });
+  })
 }
 
-var app = express();
+var app = express()
 
 app.get("/buy", function (req, res) {
   getRate(buyEndPoint, function (err, rate) {
     if (err == null) {
-      res.json({ "buy": rate });
+      res.json({ "buy": rate })
     } else {
-      res.status(500).send('Something went wrong!');
+      res.status(500).send('Something went wrong!')
     }
   })
-});
+})
 
 app.get("/sell", function (req, res) {
   getRate(sellEndPoint, function (err, rate) {
     if (err == null) {
-      res.json({ "sell": rate });
+      res.json({ "sell": rate })
     } else {
-      res.status(500).send('Something went wrong!');
+      res.status(500).send('Something went wrong!')
     }
   })
-});
+})
 
 app.listen(port, function () {
-  console.log(`listening on port: ${port} ...`);
+  console.log(`listening on port: ${port} ...`)
 })
